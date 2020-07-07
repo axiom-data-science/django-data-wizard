7dd2dcb5a6d193efde561179ef6563ae4c9bff9d
Kyle Wilcox <kyle@axiomdatascience.com>
2020-07-06 22:48:16 -0400

    Improve the list_display for File and URL sources

--- data_wizard/sources/admin.py
+++ data_wizard/sources/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from data_wizard.admin import ImportActionModelAdmin
 from .models import FileSource, URLSource
 
-admin.site.register(FileSource, ImportActionModelAdmin)
-admin.site.register(URLSource, ImportActionModelAdmin)
+
+@admin.register(FileSource)
+class FileSourceAdmin(ImportActionModelAdmin):
+    list_display = [
+        'name', 'file', 'date'
+    ]
+    list_filter = ['date']
+
+
+@admin.register(URLSource)
+class URLSourceAdmin(ImportActionModelAdmin):
+    list_display = [
+        'name', 'url', 'date'
+    ]
+    list_filter = ['date']
